fe4e8225bfc64f5abce573300de48ef1696ef55f
Park <pbk95120@gmail.com>
2023-11-14 11:47:31 +0900

    add: set basic color and test

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#111111',
+        darkgray: '#3e3e3e',
+        lightgray: '#b8b8b8',
+        textdark: '#2f2e36',
+        textgray: '#b8b8b8',
+        backgroundgray: '#f6f6f6',
+        green: '#4caf50',
+        lightgreen: '#eaf5e7',
+        gold: '#e48700',
+      },
+    },
   },
   plugins: [],
 };
