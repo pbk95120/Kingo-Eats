d91ddad72a50ec8072deddf52ebe52a2049214bd
alswns078 <alswns078@gmail.com>
2023-11-14 14:30:16 +0900

    navbar_first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button variant="primary">Primary</Button>
+      <h4> navbartest</h4>
     </div>
   );
 }
