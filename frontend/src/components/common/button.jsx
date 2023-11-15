b9e713d238c41020decd2d43bfed6286130e1682
605s <ploki7335@naver.com>
2023-11-15 11:21:57 +0900

    feat: 오타 수정

--- frontend/src/components/common/button.jsx
+++ frontend/src/components/common/button.jsx
@@ -5,7 +5,7 @@ export default function button() {
   const navigate = useNavigate();
   return (
     <button
-      className="w-[343px] h-[52px] bg-white-500 hover:shadow-md text-black font-bold rounded border-2 border-black mb-[2vh]"
+      className="w-[343px] h-[52px] bg-green hover:shadow-md text-white font-bold rounded border-2 border-black mb-[2vh]"
       onClick={() =>
         navigate(
           '원하는 경로 여기에 넣기, 모르겠으면 Mypage 폴더에 Mypage.jsx 확인하기',
