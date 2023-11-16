c17c935538df7e8e1838ab9c3a92cb8abc27cac9
B K Park <pbk95120@gmail.com>
2023-11-16 23:17:49 +0900

    feat: push title icon go back page

--- frontend/src/components/common/Title.js
+++ frontend/src/components/common/Title.js
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 const Title = ({ title }) => {
+  const navigate = useNavigate();
   return (
     <div className="relative flex w-11/12 pt-12 mx-auto justify-center items-center ">
-      <img src={process.env.PUBLIC_URL + "/images/chevron.svg"} className="absolute left-0.5" />
+      <div onClick={() => navigate(-1)} className="absolute left-0.5 cursor-pointer">
+        <img src={process.env.PUBLIC_URL + "/images/chevron.svg"} />
+      </div>
       <h1 className="text-lg text-textdark m-0 font-bold ">{title}</h1>
     </div>
   );
