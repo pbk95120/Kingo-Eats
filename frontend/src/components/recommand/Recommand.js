b129cab1f5687b8f5ef5ee5e320ecf1f2c0b9606
605s <ploki7335@naver.com>
2023-11-29 15:10:29 +0900

    fix: 메뉴 추천시 url 수정

--- frontend/src/components/recommand/Recommand.js
+++ frontend/src/components/recommand/Recommand.js
@@ -3,26 +3,39 @@ import menuData from "../../pages/menudata";
 
 const Recommand = () => {
   const restaurant = localStorage.getItem("selectedRestaurant");
-  const menuList = menuData.filter((menuItem) => menuItem.restaurantType === restaurant);
+  const menuList = menuData.filter(
+    (menuItem) => menuItem.restaurantType === restaurant
+  );
   const randomIndex = Math.floor(Math.random() * menuList.length);
   const menuItem = menuList[randomIndex];
   console.log(restaurant);
   return (
     <>
-      <img src={process.env.PUBLIC_URL + menuItem.imagePath} className="mt-3 w-full h-72 object-cover rounded" />
+      <img
+        src={process.env.PUBLIC_URL + menuItem.imagePath}
+        className="mt-3 w-full h-72 object-cover rounded"
+      />
       <div className="w-full h-80 mt-4 bg-white rounded">
-        <p className="text-sm text-textdark m-0 pl-5 pt-4">{menuItem.restaurantType}</p>
+        <p className="text-sm text-textdark m-0 pl-5 pt-4">
+          {menuItem.restaurantType}
+        </p>
         <div className="flex justify-between items-center">
-          <p className="text-2xl font-bold text-textdark m-0 pl-5">{menuItem.name}</p>
-          <p className="text-xl font-bold text-textdark m-0 pr-4">{menuItem.price}</p>
+          <p className="text-2xl font-bold text-textdark m-0 pl-5">
+            {menuItem.name}
+          </p>
+          <p className="text-xl font-bold text-textdark m-0 pr-4">
+            {menuItem.price}
+          </p>
         </div>
         <div className="flex items-center justify-end pl-2 pr-4 pb-2">
           <img src={process.env.PUBLIC_URL + "/images/추천_별.svg"} />
           <p className="text-sm text-textgray m-0">4,9</p>
         </div>
-        <p className="text-sm text-textgray m-0 px-3 py-1 pb-2">{menuItem.describe}</p>
+        <p className="text-sm text-textgray m-0 px-3 py-1 pb-2">
+          {menuItem.describe}
+        </p>
         <div className="pl-3">
-          <Btn text="구매하기" url={"/menu/1"}></Btn>
+          <Btn text="구매하기" url={`/menu/${menuItem.id}`}></Btn>
         </div>
       </div>
     </>
